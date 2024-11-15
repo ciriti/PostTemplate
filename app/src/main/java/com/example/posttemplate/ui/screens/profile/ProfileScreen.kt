e192a5b9cad68981e0e7202b20ec208efc892311
ciriti <ciriti@gmail.com>
2024-11-15 13:29:06 +0100

    first

--- app/src/main/java/com/example/posttemplate/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/posttemplate/ui/screens/profile/ProfileScreen.kt
@@ -1,20 +1,18 @@
 package com.example.posttemplate.ui.screens.profile
 
-//@Composable
-//fun ProfileScreen(
-//    state: ProfileState,
-//    onBack: () -> Unit
-//) {
-//    when {
-//        state.isLoading -> LoadingIndicator()
-//        state.errorMessage != null -> Text("Error: ${state.errorMessage}")
-//        else -> Text("Hello, ${state.user?.fullName}")
-//    }
-//}
-
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
